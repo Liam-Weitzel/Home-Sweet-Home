d9d25ba863e0428bf70ff6fcdf8aaf572ab1f956
Liam-Weitzel <liam.weitzel@gmail.com>
2023-12-01 10:40:57 +0000

    add encrypt functionality to dmenu

--- .bash_scripts/decrypt_encfs.sh
+++ .bash_scripts/decrypt_encfs.sh
@@ -1,5 +1,4 @@
 encfs ~/Desktop/Encrypted-Files/ ~/Desktop/Decrypted-Files/
 cd ~/Desktop/Decrypted-Files/
 exec bash
-sudo umount ~/Desktop/Decrypted-Files/
 
---
+++ .bash_scripts/umount_encfs.sh
@@ -0,0 +1,2 @@
+sudo umount ~/Desktop/Decrypted-Files/
+
