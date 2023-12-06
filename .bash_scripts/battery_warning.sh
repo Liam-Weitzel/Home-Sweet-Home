eb9d34f40473dbcbd7763da2e2bcc84de3dcb9f0
Liam-Weitzel <liam.weitzel@gmail.com>
2023-12-06 14:10:59 +0000

    Add battery warning pop up when below 15% and not charging

---
+++ .bash_scripts/battery_warning.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+while :
+do
+	STATE=$(acpi -b | grep "Battery 1" | grep -o "Discharging")
+	POWER=$(acpi -b | grep "Battery 0" | grep -o '[0-9]\+%' | tr -d '%')
+	if [[ $POWER -le 20 ]] && [[ $STATE = "Discharging" ]]; then
+		notify-send "Battery power is lower than 15%!"
+	fi
+sleep 30
+done
