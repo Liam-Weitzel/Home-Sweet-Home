dc64f0ca62b52d25adc8838fc2db0a1b76d59193
Liam-Weitzel <liam.weitzel@gmail.com>
2023-12-09 10:43:06 +0000

    Change acpi to read from sys to get capacity

--- .bash_scripts/battery_warning.sh
+++ .bash_scripts/battery_warning.sh
@@ -2,9 +2,9 @@
 
 while :
 do
-	STATE=$(acpi -b | grep "Battery 1" | grep -o "Discharging")
-	POWER=$(acpi -b | grep "Battery 1" | grep -o '[0-9]\+%' | tr -d '%')
-	if [[ $POWER -le 20 ]] && [[ $STATE = "Discharging" ]]; then
+	STATE=$(cat /sys/class/power_supply/BAT0/status)
+	POWER=$(cat /sys/class/power_supply/BAT0/capacity)
+	if [[ $POWER -le 15 ]] && [[ $STATE = "Discharging" ]]; then
 		notify-send "Battery power is lower than 15%!"
 	fi
 sleep 30
