fd20c85d6a77866fe11ff44728a7df24c403d4d7
Liam-Weitzel <liam.weitzel@gmail.com>
2024-01-16 17:32:14 +0000

    Remove unused bash scripts

--- .bash_scripts/grab_window_dmenu.sh
+++
@@ -1,4 +0,0 @@
-unfocused=$(i3-msg -t get_workspaces | jq '.[] | select(.focused==false).name' | cut -d '"' -f 2)
-focused=$(i3-msg -t get_workspaces | jq '.[] | select(.focused==true).name')
-workspace=$(echo "$unfocused" | dmenu -p "Grab windows from workspace: ")
-i3-msg [workspace=$workspace] move workspace $focused
