62ccfacbba030ec19b0d8d1e971687a2d87c182a
Liam-Weitzel <liam.weitzel@gmail.com>
2024-03-08 02:45:40 +0000

    Change C-q to open Trouble quickfix directly

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -566,7 +566,7 @@ vim.keymap.set('n', '<A-C-u>', '3<C-w>-', { silent = true })
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
-vim.keymap.set('n', '<leader>q', cmd 'execute "TroubleToggle" | execute "cclose"', { desc = 'Toggle last trouble window' })
+vim.keymap.set('n', '<leader>q', cmd 'execute "TroubleToggle quickfix" | execute "cclose"', { desc = 'Trouble quickfix' })
 
 -- open Neotree
 vim.keymap.set('n', '<leader>t', cmd 'Neotree toggle')
