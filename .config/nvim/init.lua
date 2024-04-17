3e228624f045159d0f4830e05d28dd1d48ff7f05
Liam-Weitzel <liam.weitzel@gmail.com>
2024-04-17 09:53:49 +0100

    Add comment to link package dependency for telescope-undo

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -148,7 +148,7 @@ require('lazy').setup({
     branch = '0.1.x',
     dependencies = {
       'nvim-lua/plenary.nvim',
-      'debugloop/telescope-undo.nvim',
+      'debugloop/telescope-undo.nvim', -- requires: https://github.com/dandavison/delta
       -- Fuzzy Finder Algorithm which requires local dependencies to be built.
       -- Only load if `make` is available. Make sure you have the system
       -- requirements installed.
